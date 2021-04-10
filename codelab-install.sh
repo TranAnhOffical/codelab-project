fa24936d986077ffce86aa59263a32dbafb01aed
Tran Anh <79737053+TranAnhOffical@users.noreply.github.com>
2021-04-10 16:03:52 +0700

    Create codelab-install.sh

---
+++ codelab-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+clear
+echo "---------- CodeLab Project 1.0.0 Beta ----------"
+echo ""
+echo "--- Install Package ---"
+apt update
+apt install sudo
+sudo apt install update
+sudo apt install wget
+clear
+echo "---------- CodeLab Project 1.0.0 Beta ----------"
+echo ""
+echo "--- Setup Codelab ---"
+wget https://raw.githubusercontent.com/TranAnhOffical/codelab-project/main/codelab.sh
+clear
+echo "---------- CodeLab Project 1.0.0 Beta ----------"
+echo ""
+ech0 "Codelab Is Installed"
+echo "Type 'bash codelab.sh' To Start Tools"
